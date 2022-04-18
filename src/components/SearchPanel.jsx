54cbc5bc1584639389b810dd8ce90b896348a73a
Oleksandr <cerebrum94@gmail.com>
2022-04-18 21:10:52 +0300

    created main veiw

---
+++ src/components/SearchPanel.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './SearchPanel.css';
+
+const SerchPanel = () => {
+  return (
+    <input type="text" 
+      className="form-control search-input"
+      placeholder="type to search" />
+  );
+}
+
+export default SerchPanel;
