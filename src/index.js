ec1db9367000cae062793ea62b752b3dd12e1083
Oleksandr <cerebrum94@gmail.com>
2022-04-19 14:09:11 +0300

    separated App component from index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,7 @@
-import React from 'react';
-// import ReactDOM from 'react-dom';
-import {
-	createRoot
-} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
-import AppHeader from './components/AppHeader';
-import SearchPanel from './components/SearchPanel';
-import ItemStatusFilter from './components/ItemStatusFilter';
-import TodoList from './components/TodoList';
+import App from './components/App';
 
 import './index.css';
-
-const App = () => {
-
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
 
 createRoot(document.getElementById('root')).render( < App /> );
