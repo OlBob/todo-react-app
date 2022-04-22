3a770f5e39aba19fe7e3c6f0d0fe7765f7470e7d
Oleksandr <cerebrum94@gmail.com>
2022-04-22 12:18:49 +0300

    added state to TodoListItem

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,32 +1,51 @@
-import React, { Component } from 'react'
-import './TodoListItem.css'
+import React, { Component } from 'react';
+import './TodoListItem.css';
 
 export default class TodoListItem extends Component {
+    state = {
+        done: false,
+        important: false,
+    };
+
     onLabelClick = () => {
         // debugger
-        console.log(`Done: ${this.props.label}`)
-    }
+        console.log(`Done: ${this.props.label}`);
+        this.setState(({ done }) => {
+            return {
+                done: !done,
+            };
+        });
+    };
+
+    onMarkImportant = () => {
+        this.setState(({ important }) => {
+            return {
+                important: !important,
+            };
+        });
+    };
 
     render() {
-        const { label, important = false } = this.props
+        const { label } = this.props;
+        const { done, important } = this.state;
 
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal',
-        }
+        let classNames = 'todo-list-item';
+
+        if (done) classNames += ' done';
+        if (important) classNames += ' important';
 
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
                 <span
                     className="todo-list-item-label"
-                    style={style}
                     onClick={this.onLabelClick}>
                     {label}
                 </span>
 
                 <button
                     type="button"
-                    className="btn btn-outline-success btn-sm float-right">
+                    className="btn btn-outline-success btn-sm float-right"
+                    onClick={this.onMarkImportant}>
                     <i className="fa fa-exclamation" />
                 </button>
 
@@ -36,6 +55,6 @@ export default class TodoListItem extends Component {
                     <i className="fa fa-trash-o" />
                 </button>
             </span>
-        )
+        );
     }
 }
